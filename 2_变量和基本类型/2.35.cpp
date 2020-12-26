35f84eb5e6ba877da0fd72f1f4a885c4fed4b66e
lance <lance@example.com>
2020-12-26 22:15:14 +0800

    20-12-26

---
+++ 2_变量和基本类型/2.35.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int main() {
+    const int i = 42;
+    auto j = i;
+    const auto &k = i;
+    auto *p = &i;
+    const auto j2 = i, &k2 = i;
+    char s = 'a';
+    std::cout << typeid(i).name() << std::endl;
+    std::cout << typeid(j).name() << std::endl;
+    std::cout << typeid(k).name() << std::endl;
+    std::cout << typeid(p).name() << std::endl;
+    std::cout << typeid(j2).name() << std::endl;
+    std::cout << typeid(s).name() << std::endl;
+    
+
+    return 0;
+}
+
